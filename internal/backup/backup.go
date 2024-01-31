557b8170f931b1a5e9dcea4584d8f041656d55d8
Siposattila <sattipolo@gmail.com>
2024-01-31 08:38:39 +0100

    scheduler test and init sepration

--- internal/backup/backup.go
+++ internal/backup/backup.go
@@ -20,29 +20,29 @@ type backup struct {
 }
 
 func NewBackup(cronExpression string, whatToBackup []string, excludeExtensions []string, excludeFiles []string) BackupInterface {
-	return backup{
+    var newBackup = backup{
 		CronExpression:    cronExpression,
 		WhatToBackup:      whatToBackup,
 		ExcludeExtensions: excludeExtensions,
 		ExcludeFiles:      excludeFiles,
 	}
-}
-
-func (b backup) BackupProcess() {
 	var cronParser = cron.NewParser(cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow)
-	var schedule, parseError = cronParser.Parse(b.CronExpression)
+	var schedule, parseError = cronParser.Parse(newBackup.CronExpression)
     if parseError != nil {
         console.Fatal("Your cron expression is invalid or an error occured: " + parseError.Error())
     }
-    b.Cron = schedule
-	console.Debug(b.Cron.Next(time.Now()).String())
+    newBackup.Cron = schedule
 
+	return newBackup
+}
+
+func (b backup) BackupProcess() {
 	var timeSignal = time.After(b.Cron.Next(time.Now()).Sub(time.Now()))
 	select {
 	case <-timeSignal:
 		// TODO: implement backup
 		console.Debug("TODO backup")
-		//BackupProcess()
+		b.BackupProcess()
 	}
 
 	return
